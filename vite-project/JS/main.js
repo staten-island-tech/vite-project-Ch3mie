80e3c953af014fd343e2dcbc449c00aba6db108f
Ch3mie <ryanc668@nycstudents.net>
2024-11-07 12:28:35 -0500

    e

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -2,12 +2,13 @@ import "../CSS/style.css";
 import { people } from "./people.js";
 import { DOMSelectors } from "./dom.js";
 
+/* const asianbtn = document.querySelector(".Asian-btn");
+
 function allcards() {
   people.forEach((person) => {
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
-      `
-  
+      ` 
           <div class="card">
           <h2>${person.name}</h2>
           <h4>${person.age}, ${person.race}</h4>
@@ -17,30 +18,90 @@ function allcards() {
     );
   });
 }
-
-function asian() {
+ function asian() {
   DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
+
     people.forEach((person) => {
-      if (person.race == "White") {
+      if (person.race === "White") {
         DOMSelectors.container.insertAdjacentHTML(
           "beforeend",
           `
-        <div class="card">
-        <h2>${person.name}</h2>
-        <h4>${person.age}, ${person.race}</h4>
-        <h4>${person.gender}| exes: ${person.exes}</h4>
-        <img class="imagesize" src="${person.image}" alt="e" />
-        </div>
+            <div class="card">
+            <h2>${person.name}</h2>
+            <h4>${person.age}, ${person.race}</h4>
+            <h4>${person.gender}| exes: ${person.exes}</h4>
+            <img class="imagesize" src="${person.image}" alt="e" />
+            </div>
 
             `
         );
       }
     });
   });
+} 
+
+function reset() {
+  document.querySelector(".container").innerHTML = "";
 }
 
-function reset() {}
-allcards();
+function main() {
+  allcards();
+  asianbtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    reset();
+    asian();
+  });
+}
+ */
 
-asian();
+const asianbtn = document.querySelector(".Asian-btn");
+
+function allcards() {
+  people.forEach((person) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      ` 
+          <div class="card">
+          <h2>${person.name}</h2>
+          <h4>${person.age}, ${person.race}</h4>
+          <h4>${person.gender} | exes: ${person.exes}</h4>
+          <img class="imagesize" src="${person.image}" alt="e" />
+          </div>     
+      `
+    );
+  });
+}
+
+function asian() {
+  people.forEach((person) => {
+    if (person.race === "Asian") {
+      DOMSelectors.container.insertAdjacentHTML(
+        "beforeend",
+        `
+            <div class="card">
+            <h2>${person.name}</h2>
+            <h4>${person.age}, ${person.race}</h4>
+            <h4>${person.gender} | exes: ${person.exes}</h4>
+            <img class="imagesize" src="${person.image}" alt="e" />
+            </div>
+        `
+      );
+    }
+  });
+}
+
+function reset() {
+  DOMSelectors.container.innerHTML = "";
+}
+
+function main() {
+  allcards();
+  asianbtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    reset();
+    asian();
+  });
+}
+
+main();
