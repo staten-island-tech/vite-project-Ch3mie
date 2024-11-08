e7ffd22c6f2f749807b91bf16eb884238976c5b1
Ch3mie <ryanc668@nycstudents.net>
2024-11-08 12:22:54 -0500

    e

--- vite-project/JS/dom.js
+++ vite-project/JS/dom.js
@@ -1,8 +1,10 @@
 const DOMSelectors = {
   h1: document.querySelector("title"),
   form: document.querySelector("form"),
-  button: document.querySelector("#asian-button"),
+  asianbutton: document.querySelector("#asian-button"),
   container: document.querySelector("#card-container"),
   card: document.querySelector(".card"),
+  malebutton: document.querySelector("#male-button"),
+  femalebutton: document.querySelector("#female-button"),
 };
 export { DOMSelectors };
--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -2,105 +2,52 @@ import "../CSS/style.css";
 import { people } from "./people.js";
 import { DOMSelectors } from "./dom.js";
 
-/* const asianbtn = document.querySelector(".Asian-btn");
-
-function allcards() {
-  people.forEach((person) => {
-    DOMSelectors.container.insertAdjacentHTML(
-      "beforeend",
-      ` 
-          <div class="card">
-          <h2>${person.name}</h2>
-          <h4>${person.age}, ${person.race}</h4>
-          <h4>${person.gender} | exes: ${person.exes}</h4>
-          <img class="imagesize" src="${person.image}" alt="e" />
-          </div>     `
-    );
-  });
-}
- function asian() {
-  DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    people.forEach((person) => {
-      if (person.race === "White") {
-        DOMSelectors.container.insertAdjacentHTML(
-          "beforeend",
-          `
-            <div class="card">
-            <h2>${person.name}</h2>
-            <h4>${person.age}, ${person.race}</h4>
-            <h4>${person.gender}| exes: ${person.exes}</h4>
-            <img class="imagesize" src="${person.image}" alt="e" />
-            </div>
-
-            `
-        );
-      }
-    });
-  });
-} 
-
-function reset() {
-  document.querySelector(".container").innerHTML = "";
-}
-
-function main() {
-  allcards();
-  asianbtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    reset();
-    asian();
-  });
-}
- */
-
 const asianbtn = document.querySelector(".Asian-btn");
+const malebtn = DOMSelectors.malebutton;
+const femalebtn = DOMSelectors.femalebutton;
 
-function allcards() {
-  people.forEach((person) => {
-    DOMSelectors.container.insertAdjacentHTML(
-      "beforeend",
-      ` 
-          <div class="card">
-          <h2>${person.name}</h2>
-          <h4>${person.age}, ${person.race}</h4>
-          <h4>${person.gender} | exes: ${person.exes}</h4>
-          <img class="imagesize" src="${person.image}" alt="e" />
-          </div>     
-      `
-    );
-  });
+function injectHTML(person) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="card">
+        <h2>${person.name}</h2>
+        <h4>${person.age}, ${person.race}</h4>
+        <h4>${person.gender} | exes: ${person.exes}</h4>
+        <img class="imagesize" src="${person.image}" alt="e" />
+      </div>
+    `
+  );
 }
 
-function asian() {
-  people.forEach((person) => {
-    if (person.race === "Asian") {
-      DOMSelectors.container.insertAdjacentHTML(
-        "beforeend",
-        `
-            <div class="card">
-            <h2>${person.name}</h2>
-            <h4>${person.age}, ${person.race}</h4>
-            <h4>${person.gender} | exes: ${person.exes}</h4>
-            <img class="imagesize" src="${person.image}" alt="e" />
-            </div>
-        `
-      );
-    }
-  });
+function displayPeople(filteredPeople) {
+  reset();
+  filteredPeople.forEach((person) => injectHTML(person));
 }
+function hightolowex() {}
 
 function reset() {
   DOMSelectors.container.innerHTML = "";
 }
 
 function main() {
-  allcards();
+  displayPeople(people);
+
   asianbtn.addEventListener("click", (event) => {
     event.preventDefault();
-    reset();
-    asian();
+    const asians = people.filter((person) => person.race === "Asian");
+    displayPeople(asians);
+  });
+
+  malebtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const males = people.filter((person) => person.gender === "Male");
+    displayPeople(males);
+  });
+  femalebtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const female = people.filter((person) => person.gender === "Female");
+    displayPeople(female);
   });
 }
 
